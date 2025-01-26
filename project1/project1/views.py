2c84610b7cb7266a43e552aa329152170a0fea34
Sushil Sharma <sharma.s420823@gmail.com>
2025-01-26 18:55:47 +0545

    mult_page

--- project1/project1/views.py
+++ project1/project1/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def Home(request):
-    return HttpResponse('Hello, World!')
+    # return HttpResponse('Hello, World!')
+    return render(request,'website/index.html')
 
 def About(request):
-    return HttpResponse("From about section")
+    # return HttpResponse("From about section")
+    return render(request,'website/about.html')
 
 def contacts(request):
-    return HttpResponse("Contact informations!!!")
+    # return HttpResponse("Contact informations!!!")
+    return render(request,'website/contact.html')
