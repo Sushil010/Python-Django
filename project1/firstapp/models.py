8f773c94c684ca66a533247cf3d3409cb2e2f757
Sushil Sharma <sharma.s420823@gmail.com>
2025-01-29 17:55:11 +0545

    model_init

--- project1/firstapp/models.py
+++ project1/firstapp/models.py
@@ -1,6 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
-class Airoplane(models.Model):
+class Aeroplane(models.Model):
+    SEAT_TYPE=[
+        ('LT','LEFT'),
+        ('MD','MIDDLE'),
+        ('RT','RIGHT')
+    ]
+
     name= models.CharField(max_length=100)
-    
+    image=models.ImageField(upload_to='pros/')
+    added_time=models.DateTimeField(default=timezone.now)
+    choices=models.CharField(max_length=2,choices=SEAT_TYPE)
