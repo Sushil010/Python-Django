dda565ab265bf5e9b2b3836072298a886b7fedb1
Sushil Sharma <sharma.s420823@gmail.com>
2025-01-31 19:31:31 +0545

    comp_relationships

--- project1/firstapp/models.py
+++ project1/firstapp/models.py
@@ -22,6 +22,41 @@ class Aeroplane(models.Model):
 
 
 # defining relationships:
+# 1. One to Many Relationship
 class Aero_review(models.Model):
-    aero=models.ForeignKey(Aeroplane,on_delete=models.CASCADE)
+    ratings=[
+        ('1','one'),
+        ('2','two'),
+        ('3','three'),
+        ('4','four'),
+        ('5','five')
+    ]
+    aero=models.ForeignKey(Aeroplane,on_delete=models.CASCADE,related_name='reviews')
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    rating=models.CharField(max_length=1,choices=ratings)
+    comment=models.TextField()
+    time_added=models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.user.username} has reviewed {self.aero.name} with rating {self.rating}'
     
+   
+# 2. Many to Many Relationship
+class Pilots(models.Model):
+    name=models.CharField(max_length=100)
+    address=models.CharField(max_length=100)
+    aeroplanes=models.ManyToManyField(Aeroplane,related_name='pilots')
+
+    def __str__(self):
+        return self.name
+
+
+# 3. One to One Relationship
+class Blackbox(models.Model):
+    blackbox=models.OneToOneField(Aeroplane,on_delete=models.CASCADE,related_name='blackbox')
+    black_id=models.CharField(max_length=100)
+    created_date=models.DateTimeField(default=timezone.now)
+    damage_date=models.DateTimeField()
+
+    def __str__(self):
+        return self.name.blackbox
