7e60f4f2f9020af40e6c6d126e1e80b609214d9d
Sushil Sharma <sharma.s420823@gmail.com>
2025-02-01 17:31:12 +0545

    admin_changeout

--- project1/firstapp/admin.py
+++ project1/firstapp/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Aeroplane
+from .models import Aero_review,Pilots,Blackbox
 
 # Register your models here.
 admin.site.register(Aeroplane)
+
+class ReviewInline(admin.TabularInline):
+    model=Aero_review
+    extra=2
+
+class AeroplaneAdmin(admin.ModelAdmin):
+    list_display=('name','added_time','choices')
