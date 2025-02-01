21668cbf02cce2abd28dc3c878cbfa74fcab19e5
Sushil Sharma <sharma.s420823@gmail.com>
2025-02-01 17:44:20 +0545

    models_added_admin

--- project1/firstapp/admin.py
+++ project1/firstapp/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from .models import Aeroplane
-from .models import Aero_review,Pilots,Blackbox
+from .models import Aero_review,Pilots, Blackbox, Aeroplane
 
 # Register your models here.
-admin.site.register(Aeroplane)
+
 
 class ReviewInline(admin.TabularInline):
     model=Aero_review
@@ -11,3 +11,17 @@ class ReviewInline(admin.TabularInline):
 
 class AeroplaneAdmin(admin.ModelAdmin):
     list_display=('name','added_time','choices')
+    inlines=[ReviewInline]
+
+
+class PilotsAdmin(admin.ModelAdmin):
+    list_display=('name','location')
+    filter_horizontal=('aeroplanes',)
+
+class BlackboxAdmin(admin.ModelAdmin):
+    list_display=('blackbox','black_id')
+
+
+admin.site.register(Aeroplane,AeroplaneAdmin)
+admin.site.register(Pilots,PilotsAdmin)
+admin.site.register(Blackbox,BlackboxAdmin)
