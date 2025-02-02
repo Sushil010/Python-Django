04a0e313ae220d43a85b6d34888d6b0df4e4b833
Sushil Sharma <sharma.s420823@gmail.com>
2025-02-02 22:49:15 +0545

    Form_initialization

--- project1/firstapp/urls.py
+++ project1/firstapp/urls.py
@@ -7,4 +7,5 @@ from . import views
 urlpatterns = [
     path('',views.first_app,name='first_app'),
     path('<int:aero_id>/',views.aero_desc,name='aero_desc'),
+    path('pil_info/',views.pil_info,name='pil_info'),
 ]
--- project1/firstapp/views.py
+++ project1/firstapp/views.py
@@ -13,4 +13,5 @@ def aero_desc(request,aero_id):
     return render(request,'firstapp/aero_desc.html',{'aero_det':aero_det})
     
 
-
+def pil_info(request):
+    return render(request,'firstapp/pil_info.html')
