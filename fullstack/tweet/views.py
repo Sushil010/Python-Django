8c34e734d773ee9efca7adeb6a0e7f0e23c1212a
Sushil Sharma <sharma.s420823@gmail.com>
2025-02-07 19:06:22 +0545

    view_changes

--- fullstack/tweet/views.py
+++ fullstack/tweet/views.py
@@ -2,6 +2,20 @@ from django.shortcuts import render
 from .models import Tweet
 from .forms import TweetForm
 
+
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+
+def tweet_lists(request):
+    tweet=Tweet.objects.all().order_by('-created_at')
+    return render(request,'tweet_list.html',{'tweet':tweet})
+
+def tweet_submit(request):
+    if request.method=='POST':
+        form=TweetForm(request.POST,request.FILES)
+        # if file is also being accepted use request.FILES in above
+    else:
+        form=TweetForm()
+    return render(request,'tweet_form.html',{'form':form})
