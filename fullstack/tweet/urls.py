5a479b78508419083d194815708630e39ea87ffd
Sushil Sharma <sharma.s420823@gmail.com>
2025-02-09 19:15:34 +0545

    Login_initialization

--- fullstack/tweet/urls.py
+++ fullstack/tweet/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # path('admin/', admin.site.urls)
     path('',views.tweet_lists,name='tweet_lists'),
     path('create/',views.tweet_create,name='tweet_create'),
+    path('register/',views.register,name='register'),
     path('<int:tweet_id>/edit/',views.tweet_edit,name='tweet_edit'),
     path('<int:tweet_id>/delete/',views.tweet_delete,name='tweet_delete'),
 ]
--- fullstack/tweet/views.py
+++ fullstack/tweet/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from .models import Tweet
-from .forms import TweetForm
+from .forms import TweetForm,UserRegistrationForm
 from django.shortcuts import get_object_or_404,redirect
 from django.contrib.auth.decorators import login_required
-
+from django.contrib.auth import login
 
 # Create your views here.
 def index(request):
@@ -68,3 +68,18 @@ def tweet_delete(request,tweet_id):
         return redirect('tweet_lists')
     return render(request,'tweet_delete_confirm.html',{'tweet':tweet})
 
+def register(request):
+    if request.method=="POST":
+        form=UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user=form.save(commit=False)
+            user.set_password(form.cleaned_data['password1'])
+            user.save()
+            login(request,user)
+            return redirect('tweet_lists')
+
+        pass
+    else:
+        form=UserRegistrationForm()
+    return render(request,'registration/register.html',{'form':form})
+    
