97f3e83e24b786ad87e10a9e45ed634149496062
Sushil Sharma <sharma.s420823@gmail.com>
2025-02-10 21:46:20 +0545

    layout_configuration

--- project2/task/urls.py
+++ project2/task/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from .import views
 
 
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
+    path('',views.index,name='index'),
+    path('task/',views.task_field,name='task')
 ]
--- project2/task/views.py
+++ project2/task/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Task
+from django.shortcuts import get_object_or_404
+
 
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def task_field(request):
+    task=Task.objects.all().order_by('-created_at')
+    return render(request,'task_page.html',{'task':task})
