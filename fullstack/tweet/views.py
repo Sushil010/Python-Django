a3d31f48903cf4a250327d2b918a96b86aa8e982
Sushil Sharma <sharma.s420823@gmail.com>
2025-02-10 20:28:59 +0545

    db_changes

--- fullstack/tweet/views.py
+++ fullstack/tweet/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import get_object_or_404,redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
 
-# Create your views here.
+
 def index(request):
     return render(request,'index.html')
 
