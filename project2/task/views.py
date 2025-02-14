3528bd875d33d92a82a86964331c95ea9ed0ee03
Sushil Sharma <sharma.s420823@gmail.com>
2025-02-14 14:35:24 +0545

    init_folder

--- project2/task/views.py
+++ project2/task/views.py
@@ -3,7 +3,7 @@ from .models import Task
 from django.shortcuts import get_object_or_404
 
 
-# Create your views here.
+# Create your views here. 
 def index(request):
     return render(request,'index.html')
 
