fdd60de056ed9be33f3e94e718cccc429877c13e
Sushil Sharma <sharma.s420823@gmail.com>
2025-02-14 19:45:25 +0545

    url_config

--- second/task/urls.py
+++ second/task/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('',views.task_list,name='task_list'),
     
 ]
