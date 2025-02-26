4ba3a280a136d2cdd5f8dbc4fe8abb8287daec84
Sushil Sharma <sharma.s420823@gmail.com>
2025-02-26 22:58:42 +0545

    uopdated_db

--- second/task/admin.py
+++ second/task/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import TododItems
+from .models import TodoItems
 
 # Register your models here.
-admin.site.register(TododItems)
+admin.site.register(TodoItems)
--- second/task/models.py
+++ second/task/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class TododItems(models.Model):
+class TodoItems(models.Model):
     title=models.CharField(max_length=100)
     completed=models.BooleanField(default=False)
 
