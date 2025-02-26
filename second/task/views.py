bb1b05cabae9eef6d718e22c5a5e21eda64e1035
Sushil Sharma <sharma.s420823@gmail.com>
2025-02-26 23:04:31 +0545

    views_update

--- second/task/views.py
+++ second/task/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
+from .models import TodoItems
 
 # Create your views here.
  
 
 def task(request):
     return render(request,'index.html')
+
+
+def todos(request):
+    items=TodoItems.objects.all()
+    return render(request,'todos.html',{'items':items})
