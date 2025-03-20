423c0f739755de5aa79bec005a169edfcfd82718
Sushil Sharma <sharma.s420823@gmail.com>
2025-03-20 15:22:43 +0545

    view_auth

--- second/task/views.py
+++ second/task/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import TodoItems
 from django.shortcuts import redirect
 from django.shortcuts import get_object_or_404,redirect
+from .forms import TodoForm, UserRegistrationForm
 
 
 # Create your views here.
@@ -14,20 +15,38 @@ def task(request):
 
 def todos(request):
     if request.method=='POST':
-        title=request.POST.get('title')
-        TodoItems.objects.create(title=title, completed=False) 
-        return redirect("todos") 
+        form=TodoForm(request.POST,request.FILES)
+        if form.is_valid():
+            tasks=form.save(commit=False)
+            tasks.user=request.user
+            tasks.save()
+            return redirect("todos")
+    
+        else:
+            form=TodoForm()
+    return render(request,'todos.html',{'form':form})
 
-    items=TodoItems.objects.all()
-    return render(request,'todos.html',{'items':items})
+    # if request.method=='POST':
+    #     title=request.POST.get('title')
+    #     TodoItems.objects.create(title=title, completed=False) 
+    #     return redirect("todos") 
+
+    # items=TodoItems.objects.all()
+    # return render(request,'todos.html',{'items':items})
 
 
 
 def deletetodo(request,todos_id):
-    task=get_object_or_404(TodoItems,pk=todos_id)
+    task=get_object_or_404(TodoItems,pk=todos_id,user=request.user)
     if request.method=='POST':
         task.delete()
         return redirect('todos')
+    return render(request,'todos.html',{'tasks':task})
+
+    # task=get_object_or_404(TodoItems,pk=todos_id)
+    # if request.method=='POST':
+    #     task.delete()
+    #     return redirect('todos')
     
-    return render(request,'todos.html')
+    # return render(request,'todos.html')
     
