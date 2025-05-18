320fab1784533f99e48695499f429ae473b5d22c
Sushil Sharma <sharma.s420823@gmail.com>
2025-05-18 23:54:43 +0545

    date_pro

--- initfile/initfile/urls.py
+++ initfile/initfile/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/',include('backnd.urls'))
+    path('home/',include('backnd.urls')),
+    path('newyear/',include('newyear.urls')),
 ]
--- initfile/newyear/views.py
+++ initfile/newyear/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+import datetime
 # Create your views here.
+
+def newresponse(request):
+    now=datetime.datetime.now()
+    return render(request,"new/inidex.html",{
+        'newyear':now.month==5 and now.day==18
+    })
