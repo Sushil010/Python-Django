b642fd0942e7c9e8b944ef102a3c7c550cdd3bd2
Sushil Sharma <sharma.s420823@gmail.com>
2025-05-19 18:46:20 +0545

    view_overload

--- initfile/tasks/views.py
+++ initfile/tasks/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Welcome to the task page")
+
+todos=['get','play','pass']
+def tasks(request):
+    return render(request,"folders/index.html",{
+        'todos':todos
+    })
