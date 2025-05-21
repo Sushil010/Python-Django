739a3fcf629508d90c1059cf60e19854da756981
Sushil Sharma <sharma.s420823@gmail.com>
2025-05-21 20:11:27 +0545

    serverside_validation

--- initfile/tasks/views.py
+++ initfile/tasks/views.py
@@ -4,7 +4,7 @@ from django import forms
 
 class NewTaskForms(forms.Form):
     text=forms.CharField(label="New Task")
-    priority=forms.IntegerField(label="Priority",max_value=10,min_value=0)
+    priority=forms.IntegerField(label="Priority",max_value=8,min_value=0)
 
 # Create your views here.
 def index(request):
@@ -17,6 +17,21 @@ def tasks(request):
     })
 
 def add_tasks(request):
+    if request.method=="POST":
+        # task=NewTaskForms() this command will just create a new blank form and to 
+        # populate the data we are gonna use request.POST this is sort of like
+        # filling or getting data into the task variable
+        form=NewTaskForms(request.POST)
+        if form.is_valid():
+            # from.cleaned_data will be used to retreive all user data 
+            # Similarly form.cleaned_data["task"] will get only the task field
+            task=form.cleaned_data["text"]
+            todos.append(task)
+        else:
+            return render(request,"folders/add.html",{
+                "form":form
+            })
+
     return render(request,"folders/add.html",{
         "form":NewTaskForms()
     })
