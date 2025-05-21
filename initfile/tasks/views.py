1cd9a484d1b15691706aea29f4f7d0795b54e59d
Sushil Sharma <sharma.s420823@gmail.com>
2025-05-21 09:58:02 +0545

    inbuilt_from

--- initfile/tasks/views.py
+++ initfile/tasks/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django import forms
+
+class NewTaskForms(forms.Form):
+    text=forms.CharField(label="New Task")
 
 # Create your views here.
 def index(request):
@@ -12,11 +16,13 @@ def tasks(request):
     })
 
 def add_tasks(request):
+    return render(request,"folders/add.html",{
+        "form":NewTaskForms()
+    })
+    # if request.method=="POST":
+    #     tasks=request.POST.get("task")
+    #     if tasks:
+    #         todos.append(tasks)
+    #     return redirect('add_tasks')
     # return render(request,"folders/add.html")
-    if request.method=="POST":
-        tasks=request.POST.get("task")
-        if tasks:
-            todos.append(tasks)
-        return redirect('add_tasks')
-    return render(request,"folders/add.html")
     # return redirect('todos')
