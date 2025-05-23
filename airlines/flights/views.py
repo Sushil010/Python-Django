c739a4098b23570a5eb974571d4703854e88f146
Sushil Sharma <sharma.s420823@gmail.com>
2025-05-23 16:16:18 +0545

    added_exist_models

--- airlines/flights/views.py
+++ airlines/flights/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Flights
 
 # Create your views here.
+def flight(request):
+    return render(request,"flights/index.html",{
+        "flights":Flights.objects.all()
+    })
