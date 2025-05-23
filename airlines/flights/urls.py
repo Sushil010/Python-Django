c6b20df4fae116ce1cbaba430cf351c2cee82faa
Sushil Sharma <sharma.s420823@gmail.com>
2025-05-23 16:38:18 +0545

    route_defined

--- airlines/flights/urls.py
+++ airlines/flights/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns=[
-    path('',views.flight,name="flight")
+    path('',views.flight,name="flight"),
+    path('<int:flight_id>',views.getDetails,name="get_details")
 ]
--- airlines/flights/views.py
+++ airlines/flights/views.py
@@ -6,3 +6,9 @@ def flight(request):
     return render(request,"flights/index.html",{
         "flights":Flights.objects.all()
     })
+
+def getDetails(request,flight_id):
+    flight=Flights.objects.get(pk=flight_id)
+    return render(request,"flights/planes.html",{
+        "flight":flight
+    })
