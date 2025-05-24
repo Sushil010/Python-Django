ead71e8eea95fef17335c41e24b7014cb19d8b78
Sushil Sharma <sharma.s420823@gmail.com>
2025-05-24 19:09:15 +0545

    addon_model

--- airlines/flights/admin.py
+++ airlines/flights/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Airport, Flights
+from .models import Airport, Flights,Passangers
 
 # Register your models here.
 admin.site.register(Airport)
 admin.site.register(Flights)
+admin.site.register(Passangers)
--- airlines/flights/models.py
+++ airlines/flights/models.py
@@ -17,3 +17,11 @@ class Flights(models.Model):
 
     def __str__(self):
         return (f"{self.id}. From {self.origin} to {self.destination } within {self.duration} minutes")
+    
+
+class Passangers(models.Model):
+    first=models.CharField(max_length=64)
+    last=models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.first}{self.last}"
