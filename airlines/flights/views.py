e8c936c39bf8e016b472c8f2fabafa5089259d3c
Sushil Sharma <sharma.s420823@gmail.com>
2025-05-25 19:04:11 +0545

    retreive_p_data

--- airlines/flights/views.py
+++ airlines/flights/views.py
@@ -4,11 +4,12 @@ from .models import Flights
 # Create your views here.
 def flight(request):
     return render(request,"flights/index.html",{
-        "flights":Flights.objects.all()
+        "flights":Flights.objects.all(),
     })
 
 def getDetails(request,flight_id):
     flight=Flights.objects.get(pk=flight_id)
     return render(request,"flights/planes.html",{
-        "flight":flight
+        "flight":flight,
+        "passangers":flight.passangers.all()
     })
