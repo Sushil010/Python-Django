35b38d3fbdcd275828a9b188e369562486ee1cb9
Sushil Sharma <sharma.s420823@gmail.com>
2025-05-26 18:55:17 +0545

    book_form

--- airlines/flights/views.py
+++ airlines/flights/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Flights
+from .models import Flights,Passangers
 
 # Create your views here.
 def flight(request):
@@ -11,7 +11,9 @@ def getDetails(request,flight_id):
     flight=Flights.objects.get(pk=flight_id)
     return render(request,"flights/planes.html",{
         "flight":flight,
-        "passangers":flight.passangers.all()
+        "passangers":flight.passangers.all(),
+        "non_passanger":Passangers.objects.exclude(flights=flight).all()
+
     })
 
 def book_flight(request,flight_id):
