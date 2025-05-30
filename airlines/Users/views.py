0f49ac80f2fe62b7d1d6c9d99c691a962569dcc1
Sushil Sharma <sharma.s420823@gmail.com>
2025-05-30 19:43:51 +0545

    login_redirect

--- airlines/Users/views.py
+++ airlines/Users/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def main_page(request):
-    return render(request,"users/index.html")
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('login')) 
+    # return render(request,"users/index.html")
     # return HttpResponse("welecome to the page")
 
 
 def login_method(request):
-    if request.method=="POST":
-        pass
+    return render(request,'users/index.html')
+    # if request.method=="POST":
+    #     pass
 
 def logout_method(request):
     if request.method=="POST":
