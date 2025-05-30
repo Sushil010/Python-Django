7558c26d08a9bef3ced03849e0d9c419b93b5f79
Sushil Sharma <sharma.s420823@gmail.com>
2025-05-30 12:23:53 +0545

    user_homepage

--- airlines/Users/views.py
+++ airlines/Users/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def main_page(request):
+    return render(request,"users/index.html")
+    # return HttpResponse("welecome to the page")
