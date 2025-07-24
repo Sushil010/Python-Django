6d4ee4b3a43263fe12099ac6c9ee6886893461bc
Sushil Sharma <sharma.s420823@gmail.com>
2025-07-24 17:50:54 +0545

    view_model

--- destructnotes/notes/views.py
+++ destructnotes/notes/views.py
@@ -1,45 +1,58 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from .models import Notes 
 import uuid
 import datetime
 
 # Create your views here.
-note_Store={}
+# note_Store={}
+# def note_form(request):
+#     if request.method=="POST":
+#         content=request.POST.get("note")
+#         note_id=str(uuid.uuid4())
+#         time_stamp=datetime.datetime.now()
+
+#         note_Store[note_id]={
+#             "content":content,
+#             "time_stamp":time_stamp,
+#             "viewed":False
+#         }
+
+#         return render(request,"notes/share_url.html",{"note_id":note_id})
+
+#     return render(request, "notes/form.html")
+
+
 def note_form(request):
     if request.method=="POST":
-        content=request.POST.get("note")
-        note_id=str(uuid.uuid4())
-        time_stamp=datetime.datetime.now()
+        content=request.POST.get("note") ##note is a variable stored in form 
+        note=Notes.objects.create(content=content)
 
-        note_Store[note_id]={
-            "content":content,
-            "time_stamp":time_stamp,
-            "viewed":False
-        }
+        return render(request,"notes/share_url.html",{"note_id":note.note_id})
+    return render(request,"notes/form.html")
 
-        return render(request,"notes/share_url.html",{"note_id":note_id})
 
-    return render(request, "notes/form.html")
 
 
 def view_note(request,note_id):
-    note=note_Store.get(note_id)
-    # note={
-    # "content": "hello world",
-    # "time_stamp": datetime.datetime(2025, 7, 23, 22, 15),
-    # "viewed": False
-    # }
+    pass
+    # note=note_Store.get(note_id)
+    # # note={
+    # # "content": "hello world",
+    # # "time_stamp": datetime.datetime(2025, 7, 23, 22, 15),
+    # # "viewed": False
+    # # }
     
-    if not note:
-        return HttpResponse("Note not found")
+    # if not note:
+    #     return HttpResponse("Note not found")
     
-    now=datetime.datetime.now()
-    diff=(now-note['time_stamp']).total_seconds()
-    if note["viewed"]==True or diff>300:
-        return HttpResponse("Note has expired")
+    # now=datetime.datetime.now()
+    # diff=(now-note['time_stamp']).total_seconds()
+    # if note["viewed"]==True or diff>300:
+    #     return HttpResponse("Note has expired")
         
-    note["viewed"]=True
-    return HttpResponse(f"Note:{note['content']}")
+    # note["viewed"]=True
+    # return HttpResponse(f"Note:{note['content']}")
 
 
 
