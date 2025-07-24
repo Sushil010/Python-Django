9f88b3f7d5419f9e118d10ebd239c12df47d04d6
Sushil Sharma <sharma.s420823@gmail.com>
2025-07-24 15:11:24 +0545

    form_init

--- destructnotes/notes/urls.py
+++ destructnotes/notes/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # path('admin/',)
     path('home/', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact'),
+    # path('about/', views.about, name='about'),
+    # path('contact/', views.contact, name='contact'),
 ]
+
--- destructnotes/notes/views.py
+++ destructnotes/notes/views.py
@@ -4,13 +4,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-#   return HttpResponse("<h1>Welcome to notes's page: Home page</h1>")
-    return render(request, "notes/home.html")
+    return render(request, "notes/form.html")
 
-def about(request):
-#   return HttpResponse("<h1>Welcome to notes's page: About page</h1>")
-    return render(request,"notes/about.html")
+# def about(request):
+# #   return HttpResponse("<h1>Welcome to notes's page: About page</h1>")
+#     return render(request,"notes/about.html")
 
-def contact(request):
-#   return HttpResponse("<h1>Welcome to notes's page: Contact page</h1>")
-    return render(request,"notes/contact.html")
+# def contact(request):
+# #   return HttpResponse("<h1>Welcome to notes's page: Contact page</h1>")
+#     return render(request,"notes/contact.html")
