11a8283817f5428c083381e581fbfd0f415687ca
Sushil Sharma <sharma.s420823@gmail.com>
2025-07-24 17:09:23 +0545

    added_model

--- destructnotes/notes/models.py
+++ destructnotes/notes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Notes(models.Model):
+    note_id= models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    content=models.TextField()
+    created_at=models.DateTimeField(auto_now=True)
+    viewed=models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.note_id)
