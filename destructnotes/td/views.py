677c1fc78eb0f3f15061baee0e93f98a2a9d71c4
Sushil Sharma <sharma.s420823@gmail.com>
2025-07-25 23:20:08 +0545

    model_addon

--- destructnotes/td/views.py
+++ destructnotes/td/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def fir(request):
-    return HttpResponse("Welcome to todos")
+def forms(request):
+    if request.method=="post":
+        pass
+    # return render(request,"td/form.html")
