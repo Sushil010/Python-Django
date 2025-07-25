a384b9d702a38048a45983e117c231eda866a50e
Sushil Sharma <sharma.s420823@gmail.com>
2025-07-25 23:37:56 +0545

    view_update

--- destructnotes/td/models.py
+++ destructnotes/td/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Notes(models.Model):
+    id=models.AutoField(primary_key=True)
+    text=models.CharField()
+    created_at=models.DateTimeField(auto_now=True)
--- destructnotes/td/views.py
+++ destructnotes/td/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
+from .models import Notes
 
 # Create your views here.
 def forms(request):
     if request.method=="post":
-        pass
-    # return render(request,"td/form.html")
+        datas=request.POST.get("tasks")
+        notes=Notes.objects.create(text=datas)
+
+        return HttpResponse("Submitted Successfully!!")
+    return render(request,"td/form.html")
