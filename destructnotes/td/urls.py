a9912375a2dbc4fd373805ce0561a17b49c265bf
Sushil Sharma <sharma.s420823@gmail.com>
2025-07-27 18:10:58 +0545

    managed_tasks

--- destructnotes/td/urls.py
+++ destructnotes/td/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.forms,name="forms"),
-    path('delete/<int:ids>',views.delete_tasks,name='delete_tasks')
+    path('delete/<int:ids>',views.delete_tasks,name='delete_tasks') #this ids variable
+    # should match what we are passing to delete_tasks in views
+    
     # path('lists/')
 ]
