f2f8674154a43d9e0f041ff4d10d89e73b194944
Sushil Sharma <sharma.s420823@gmail.com>
2025-07-27 19:00:53 +0545

    edit_format

--- destructnotes/td/views.py
+++ destructnotes/td/views.py
@@ -22,14 +22,14 @@ def delete_tasks(request,ids):
     return redirect("forms")
 
 def edit_tasks(request,ids):
-    to_be_updated=get_object_or_404(Notes,id=ids)
+    to_be_updated=get_object_or_404(Notes,id=ids)# gather data.1
 
-    if request.method=="post":
-        new_text=request.method.get("edit_text")
+    if request.method=="POST":#unless edit button is pressed won't execute2.
+        new_text=request.POST.get("edit_text")#4.....
         if new_text.strip():
-            to_be_updated=new_text
+            to_be_updated.text=new_text
             to_be_updated.save()
             return redirect("forms")
     
-    return render(request,"td/edit.html",{'to_update':to_be_updated})
+    return render(request,"td/edit.html",{'to_update':to_be_updated})#3.
 
