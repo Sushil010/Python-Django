321d65e66ed04308d23fce941e9d6a6e455c5aca
Sushil Sharma <sharma.s420823@gmail.com>
2025-07-30 20:08:11 +0545

    delete_included

--- destructnotes/blog/urls.py
+++ destructnotes/blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.main,name="main"),
-    path('create/',views.create_post,name="create_post")
+    path('create/',views.create_post,name="create_post"),
+    path('delete/<int:idx>',views.delete_posts,name="delete_post")
 ]
--- destructnotes/blog/views.py
+++ destructnotes/blog/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse
 from .models import blog
 
 # Create your views here.
 def main(request):
     vals=blog.objects.all()
-    return render(request,"blog/homepage.html",{"vals":vals})
+    total_posts=vals.count()
+
+    return render(request,"blog/homepage.html",{
+        "vals":vals,
+        "total_posts":total_posts
+        })
 
 
 def create_post(request):
@@ -14,6 +19,7 @@ def create_post(request):
         content=request.POST.get("content")
         if title.strip() and content.strip():
             blog.objects.create(title=title,content=content)
+        
         return redirect("main",
                     #   {'title':title,
                     #     'content':content
@@ -23,3 +29,14 @@ def create_post(request):
     # return HttpResponse("Start creating")
     return render(request,"blog/create.html")
     # if request.method="POST":
+
+
+def delete_posts(request,idx):
+    posts=get_object_or_404(blog,id=idx)
+    posts.delete()
+    return redirect("main")
+
+def edit_posts(request,idx):
+    editable=get_object_or_404(blog,id=idx)
+
+    return render(request,"")
